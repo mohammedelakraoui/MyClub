c77968225957531882b829edbc906f67fbf9048d
mohammedelakraoui <mohammed.elakraoui@gmail.com>
2014-08-21 23:27:05 +0200

    Maps

--- app/src/main/java/fr/esgi/mymodule/mymodule/myclub/app/Maps/MapsActivity.java
+++ app/src/main/java/fr/esgi/mymodule/mymodule/myclub/app/Maps/MapsActivity.java
@@ -1,47 +1,88 @@
 package fr.esgi.mymodule.mymodule.myclub.app.Maps;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import fr.esgi.mymodule.mymodule.myclub.app.R;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements LocationListener, LocationSource{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private LocationSource.OnLocationChangedListener mListener;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (locationManager != null) {
+            boolean gpsIsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean networkIsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (gpsIsEnabled) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 10F, (android.location.LocationListener) this);
+            }
+            if (networkIsEnabled) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000L, 10F, (android.location.LocationListener) this);
+            }
+
+        }
+
         setUpMapIfNeeded();
+
+    }
+
+
+    @Override
+    public void onPause()
+    {
+        if(locationManager != null)
+        {
+            locationManager.removeUpdates(this);
+        }
+
+        super.onPause();
     }
 
     @Override
-    protected void onResume() {
+    public void onLocationChanged(Location location)
+    {
+        if( mListener != null )
+        {
+            mListener.onLocationChanged( location );
+
+            //Move the camera to the user's location once it's available!
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+        }
+    }
+    @Override
+    public void onResume()
+    {
         super.onResume();
+
         setUpMapIfNeeded();
+
+        if(locationManager != null)
+        {
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
+
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -55,13 +96,52 @@ public class MapsActivity extends FragmentActivity {
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
+
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+       // 4-6 Rue Louis Armand Paris ; 48.8331220	2.2775678
+        //111 Avenue Victor Hugo Aubervilliers; 48.9070929	2.3762860
+        //111 Avenue Victor Hugo 93300 Aubervilliers; 48.9070929	2.3762860
+        mMap.setMyLocationEnabled(true);
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(48.8331220,2.2775678)).title("4-6 Rue Louis Armand Paris"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(48.9070929,2.3762860)).title("111 Avenue Victor Hugo Aubervilliers"));
+       // mMap.addMarker(new MarkerOptions().position(new LatLng(48.9070929,	2.3762860)).title("4-6 Rue Louis Armand Paris"));
+    }
+
+
+
+    @Override
+    public void activate(OnLocationChangedListener listener)
+    {
+        mListener = listener;
+    }
+
+    @Override
+    public void deactivate()
+    {
+        mListener = null;
+    }
+
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, "provider disabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, "provider enabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, "status changed", Toast.LENGTH_SHORT).show();
     }
 }
